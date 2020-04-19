9e1fc6278e45673e6874629487019f104d46da59
chyzheng <chyzheng@tencent.com>
2020-04-19 18:27:53 +0800

    rotated

--- algorithms/033_search_in_rotated/main.go
+++ algorithms/033_search_in_rotated/main.go
@@ -3,28 +3,53 @@ package main
 import "fmt"
 
 func search(nums []int, target int) int {
-	value := 0
-	if target > nums[0]{
-
-	} else {
-		
+	if len(nums) < 1 {
+		return -1
 	}
 	l := 0
 	r := len(nums) - 1
-	for l < r {
+	for l <= r {
+		if target < nums[l] && target > nums[r] {
+			return -1
+		}
 		mid := (l + r) / 2
-		if target > l {
-			if 
+		if nums[l] == target {
+			return l
+		} else if nums[l] > target {
+			if nums[r] == target {
+				return r
+			}
+			if nums[mid] > nums[l] {
+				l = mid + 1
+			} else {
+				if nums[mid] == target {
+					return mid
+				} else if nums[mid] > target {
+					r = mid - 1
+				} else {
+					l = mid + 1
+				}
+			}
 		} else {
-
+			if nums[mid] < nums[l] {
+				r = mid - 1
+			} else {
+				if nums[mid] == target {
+					return mid
+				} else if nums[mid] > target {
+					r = mid - 1
+				} else {
+					l = mid + 1
+				}
+			}
 		}
 	}
-	return value
+	return -1
 }
 
 func main() {
-	nums := []int{4, 5, 6, 7, 0, 1, 2}
+	nums := []int{1}
 	fmt.Printf("nums:%+v\n", nums)
-	value := search(nums, 0)
+	value := search(nums, 1)
 	fmt.Printf("value:%d\n", value)
 }
