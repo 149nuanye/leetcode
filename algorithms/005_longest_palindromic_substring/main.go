c9e7c570d44a5bb352419989047d27bf39ff7a20
chyzheng <chyzheng@tencent.com>
2020-05-24 17:22:45 +0800

    005

--- algorithms/005_longest_palindromic_substring/main.go
+++ algorithms/005_longest_palindromic_substring/main.go
@@ -4,55 +4,42 @@ import (
 	"fmt"
 )
 
-func longestPalindrome(s string) string {
+func ifPalindrome(s string) bool {
 	if len(s) <= 1 {
-		return s
+		return true
 	}
-	longestStr := make([]byte, 0, len(s))
-	lStr := make([]byte, 0, (len(s)+1)/2)
-	rStr := make([]byte, 0, (len(s)+1)/2)
-	begin := true
-	for lindex := 0; lindex < len(s); lindex++ {
-		if begin {
-			lStr = lStr[:0]
-			rStr = rStr[:0]
-			rStr = append(rStr, s[lindex])
-			begin = false
+	lindex := 0
+	rindex := len(s) - 1
+	for lindex < rindex {
+		if s[lindex] != s[rindex] {
+			return false
 		}
-		if lindex+1 < len(s) {
-			if s[lindex] == s[lindex+1] {
-				rStr = append(rStr, s[lindex+1])
+		lindex++
+		rindex--
+	}
+	return true
+}
+
+func longestPalindrome(s string) string {
+	longerStr := ""
+	for index := 0; index < len(s); index++ {
+		for j := index + 1; j <= len(s); j++ {
+			tmpStr := string(s[index:j])
+			if len(tmpStr) < len(longerStr) {
 				continue
 			}
-		}
-		begin = true
-
-		i := lindex - len(rStr)
-		j := lindex + 1
-		for i >= 0 && j < len(s) {
-			if s[i] == s[j] {
-				rStr = append(rStr, s[j])
-				lStr = append(lStr, s[i])
-			} else {
-				break
+			if ifPalindrome(tmpStr) {
+				if len(tmpStr) > len(longerStr) {
+					longerStr = tmpStr
+				}
 			}
-			i--
-			j++
-		}
-		// fmt.Printf("lStr:%s,rStr:%s,longestStr:%s\n", rStr, longestStr)
-		if len(lStr)+len(rStr) > len(longestStr) {
-			longestStr = longestStr[:0]
-			for index := len(lStr) - 1; index >= 0; index-- {
-				longestStr = append(longestStr, lStr[index])
-			}
-			longestStr = append(longestStr, rStr...)
 		}
 	}
-	return string(longestStr)
+	return longerStr
 }
 
 func main() {
 	str1 := "abadd"
 	palindrome := longestPalindrome(str1)
-	fmt.Printf("str1:%s,palindrome:%s\n", str1, palindrome)
+	fmt.Printf("str1:%s, palindrome:%s\n", str1, palindrome)
 }
